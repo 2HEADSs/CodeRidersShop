316b1a85287b855538712527388e6f4e66e5008d
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-13 22:34:07 +0300

    add header component and fix main css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
-    <div className='main-container'>
-      <h1>Code Rider Shop</h1>
-    </div>
+    //to remove css from root element if i want a navigation which will 
+    //be hidden when scroling
+    <>
+      <Header />
+      <h1>Test Heading</h1>
+    </>
   )
 }
 
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <h2>
+                <a className='home-na-link' to="/">
+                    CoderRiderShop
+                </a>
+            </h2>
+        </header>
+    );
+}
+
