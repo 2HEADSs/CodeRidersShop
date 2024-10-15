9f2750543970940a7b4bcec0a67f1742f8e550c5
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-15 16:28:30 +0300

    create simple navigation bar with css

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,19 @@
+import './Header.css';
+
 export default function Header() {
     return (
-        <header>
-            <h2>
-                <a className='home-na-link' to="/">
-                    CoderRiderShop
-                </a>
-            </h2>
+        <header className="header">
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <a href="/" className="nav-item">Home</a>
+                </div>
+                <div className="navbar-right">
+                    <a href="/login" className="nav-item">Login</a>
+                    <a href="/register" className="nav-item">Register</a>
+                    <a href="/catalog" className="nav-item">Catalog</a>
+                    <a href="/user" className="nav-item">User</a>
+                </div>
+            </nav>
         </header>
     );
 }
