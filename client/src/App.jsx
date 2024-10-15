d2f95f798b7e252aa316390d9edbf91aa3503c89
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-15 23:22:22 +0300

    Revert "create Login component with simple css"
    This reverts commit 92dc1c8fbb751d4abb23beb4105d2aa3047876e7.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/header/home/Home'
-import Login from './components/header/login/Login'
 
 function App() {
 
@@ -13,7 +12,6 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
       </Routes>
       <h1>Test Heading</h1>
     </>
