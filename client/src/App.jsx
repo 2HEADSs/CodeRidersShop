a55670bc7353679c85e1af7f8ef9bdec8baf4abe
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-17 21:55:43 +0300

    create User and Catalog components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
+import Catalog from './components/catalog/Catalog'
+import User from './components/user/User'
 
 function App() {
 
@@ -12,12 +14,14 @@ function App() {
     //be hidden when scroling
     <>
       <Header />
+      <h1>Test Heading</h1>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/catalog' element={<Catalog />} />
+        <Route path='/user' element={<User />} />
       </Routes>
-      <h1>Test Heading</h1>
     </>
   )
 }
---
+++ client/src/components/catalog/Catalog.jsx
@@ -0,0 +1,7 @@
+export default function Catalog() {
+    return (
+        <>
+            <h2>Catalog component</h2>
+        </>
+    );
+}
