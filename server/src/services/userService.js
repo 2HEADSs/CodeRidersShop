cd9d334c1a88defcc6e36caac0424db0a368e961
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-18 17:57:24 +0300

    create register service

---
+++ server/src/services/userService.js
@@ -0,0 +1,15 @@
+import User from "../models/User.js"
+
+async function registerUser(requestBody) {
+
+    const email = requestBody.email;
+
+    const hashedPassword = requestBody.hashedPassword;
+
+    const user = await User.create({ email, hashedPassword });
+    console.log(`User from Service ${user}`);
+
+    return user;
+}
+
+export { registerUser }
