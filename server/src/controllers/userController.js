5ec0e3ba1cb83c0d95d1150bd3b6b99be26f9824
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-18 17:58:49 +0300

    create register user controller

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,9 +1,24 @@
 import express from 'express';
 const userRouter = express.Router();
-
+import { registerUser } from '../services/userService.js';
 
 userRouter.get('/profile', (req, res) => {
     res.json({ message: 'User profile' });
+});
+
+userRouter.post('/register', async (req, res) => {
+    console.log(req.body);
+    try {
+        const user = await registerUser(req.body)
+        console.log(`User from Controlles ${user}`);
+        res.status(200);
+        res.send(user)
+        res.end()
+    } catch (error) {
+        console.log(error.message);
+
+    }
+
 });
 
 
