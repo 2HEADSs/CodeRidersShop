fed38b596feb484fe6e2e379c96d660b9e5ccb32
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-20 12:27:57 +0300

    implement password hashing when register

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,14 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import User from "../models/User.js"
+import jwt from 'jsonwebtoken'
+const secretForAccessToken = process.env.AUTH_TOKEN_SECRET
 
-async function registerUser(requestBody) {
 
-    const email = requestBody.email;
-    const hashedPassword = requestBody.hashedPassword;
+async function registerUser(userData) {
 
-    const user = await User.create({ email, hashedPassword });
+    // const email = requestBody.email;
+    // const hashedPassword = requestBody.hashedPassword;
+
+    const user = await User.create(userData);
     console.log(`User from Service ${user}`);
+    const accessToken = createAccessToken(user)
 
-    return user;
+    return { user, accessToken };
+}
+
+function createAccessToken(user) {
+    const payload = {
+        _id: user._id,
+        email: user.email
+    }
+
+    return jwt.sign(payload, secretForAccessToken)
 }
 
 export { registerUser }
