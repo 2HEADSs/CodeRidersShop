166cfd32344326b3c24e1e938b481bbdd54ab572
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-24 22:52:40 +0300

    add requesterId to request using util Function

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import router from './src/router.js'
 import cors from './src/middlewares/cors.js';
 import sanitize from './src/middlewares/sanitize.js';
+import jwtParser from './src/utils/jwtParser.js';
 
 
 
@@ -25,6 +26,7 @@ function serverStart() {
     app.use(express.json())
     app.use(cors())
     app.use(sanitize)
+    app.use(jwtParser)
     app.use(router)
     app.listen(3000, () => console.log('Server is running on port 3000'));
 }
---
+++ server/src/utils/jwtParser.js
@@ -0,0 +1,16 @@
+import parseToken from "../middlewares/authMiddleware.js";
+
+export default function jwtParser(req, res, next) {
+    const token = req.headers['x-authorization'];
+
+    if (token) {
+        try {
+            const tokenData = parseToken(token);
+            req.requesterId = tokenData._id;
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid authorization token' });
+        }
+    }
+    next();
+}
+
