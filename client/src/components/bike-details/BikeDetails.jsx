74d90c408de87a2a6dd073f7cb8fc78295825447
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-10-31 21:47:12 +0200

    add function to format the createdAt date

--- client/src/components/bike-details/BikeDetails.jsx
+++ client/src/components/bike-details/BikeDetails.jsx
@@ -5,7 +5,7 @@ import { getOne } from '../../api/bike-api';
 import { useParams } from 'react-router-dom';
 
 function BikeDetails() {
-    const [bike, setBike] = useState({});
+    const [bike, setBike] = useState({ owner: {} });
     const { bikeId } = useParams();
 
     useEffect(() => {
@@ -27,11 +27,18 @@ function BikeDetails() {
                 <p><strong>Year:</strong> {bike.year}</p>
                 <p><strong>Condition:</strong> {bike.used ? 'Used' : 'New'}</p>
                 <p><strong>Description:</strong> {bike.description || "No additional details available."}</p>
-                <p><strong>Contacts:</strong> {bike.owner.email}</p>
+                <p><strong>Contact with owner:</strong> {bike.owner.email}</p>
+                <p><strong>Created:</strong> {formatCreatedAt(bike.createdAt)}</p>
 
             </div>
         </div>
     );
+}
+
+function formatCreatedAt(dateString) {
+    return new Date(dateString).toLocaleString('en-US', {
+        year: 'numeric', month: 'long', day: 'numeric'
+    });
 }
 
 export default BikeDetails;
