c74843f818eb59b9af1af6936d2bfe1d113017f1
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-11-03 16:35:21 +0200

    remove unnecessary try-catch

--- client/src/api/bike-api.js
+++ client/src/api/bike-api.js
@@ -3,43 +3,32 @@ import requester from "./requester";
 const BASE_URL = 'http://localhost:3000/bikes';
 
 const getAllBikes = async () => {
-    try {
-        const result = await requester.get(BASE_URL);
-        return result;
-    } catch (error) {
-        return error
-    }
+    const result = await requester.get(BASE_URL);
+    return result;
+    // try {
+    //     const result = await requester.get(BASE_URL);
+    //     return result;
+    // } catch (error) {
+    //     return error
+    // }
 
 
 };
 
 const getLastAdded = async () => {
-    try {
-        const result = await requester.get(`${BASE_URL}/lastAdded`);
-        return result;
-    } catch (error) {
-        return error
-    }
-
+    const result = await requester.get(`${BASE_URL}/lastAdded`);
+    return result;
 };
 
 
 const getOne = async (bikeId) => {
-    try {
-        const result = await requester.get(`${BASE_URL}/${bikeId}`);
-        return result;
-    } catch (error) {
-        return error
-    }
+    const result = await requester.get(`${BASE_URL}/${bikeId}`);
+    return result;
 };
 
 const create = async (bikeData) => {
-    try {
-        const result = await requester.post(`${BASE_URL}/create`, bikeData);
-        return result
-    } catch (error) {
-        return error
-    }
+    const result = await requester.post(`${BASE_URL}/create`, bikeData);
+    return result
 };
 
 export {
