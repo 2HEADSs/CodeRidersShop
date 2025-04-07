ef366645f0886796197430c370c466ed88c80a31
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-04-07 23:40:27 +0300

    Create userService.ts

---
+++ server/src/services/userService.ts
@@ -0,0 +1,106 @@
+
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs'
+import prisma from '../prismaClient';
+import { UserDataRegisterService } from '../types';
+
+import dotenv from 'dotenv';
+import { log } from 'console';
+dotenv.config();
+const secretForAccessToken = process.env.AUTH_TOKEN_SECRET;
+
+
+
+
+
+async function registerUser(userData: UserDataRegisterService) {
+    try {
+        const { id, email, firstName, lastName, phone, createdAt, updatedAt } = await prisma.user.create({
+            data: {
+                email: userData.email,
+                firstName: userData.firstName,
+                lastName: userData.lastName,
+                phone: userData.phone,
+                hashedPassword: userData.hashedPassword
+            }
+        });
+        const userWithoutPassword = { id, email, firstName, lastName, phone, createdAt, updatedAt };
+
+        const accessToken = createAccessToken({ id, email });
+        console.log(userWithoutPassword);
+
+
+
+        return Object.assign({ user: userWithoutPassword }, { accessToken })
+
+    } catch (error: any) {
+        console.log(error.message)
+        //TODO: Change error message
+        throw new Error("Wrong email or password!");
+    }
+}
+
+
+async function loginUser(email: string, password: string) {
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+            include: {
+                ownedBikes: {
+                    include: {
+                        owner: true,
+                        likedByUsers: true,
+                    },
+                },
+            },
+        });
+        if (!user) {
+            throw new Error('Wrong email or password!');
+        }
+
+        const comparePassword = bcrypt.compare(password, user.hashedPassword);
+
+        if (!comparePassword) {
+            throw new Error('Wrong email or password!');
+        }
+
+        const accessToken = createAccessToken({ id: user.id, email: user.email });
+        console.log(user);
+
+        const { hashedPassword, ...safeUser } = user;
+
+        return {
+            ...safeUser,
+            accessToken,
+        };
+
+    } catch (error: any) {
+        throw new Error("Wrong email or password!");
+    }
+
+
+
+}
+
+
+function createAccessToken(user: {
+    id: string,
+    email: string
+}) {
+    const payload = {
+        id: user.id,
+        email: user.email
+    }
+    if (!secretForAccessToken) {
+        throw new Error('AUTH_TOKEN_SECRET is not defined');
+    }
+
+    return jwt.sign(payload, secretForAccessToken)
+}
+
+export {
+    registerUser,
+    loginUser,
+    // getOneUser
+}
